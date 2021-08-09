889913c2cbb246cf6fd6e9cddbd09048ba4ceab4
rajshetye11 <rajshetye11@gmail.com>
2021-08-09 18:50:56 +0530

    [raj] : added the position of player at start

--- src/com/bridgelabz/snakeandladder/SnakeAndLadderComputation.java
+++ src/com/bridgelabz/snakeandladder/SnakeAndLadderComputation.java
@@ -1,10 +1,26 @@
 package com.bridgelabz.snakeandladder;
 
+import java.util.Random;
+
 public class SnakeAndLadderComputation 
 {
 	public static void main(String[] args) {
 
 		System.out.println("Welcome To Snake and Ladder");
+		
+		String player1 = "Raj";
+		int position = 0, start_position = 0, max_position = 100;
+		
+		
+		System.out.println();
+		System.out.println("Name of the Player : "+player1);			//UC1
+		System.out.println("Starting Position : " +start_position);		//UC1
+		System.out.println("End Position : " +max_position);			//UC1
+		System.out.println("Player Position : " +position);             //UC1
+		
+		
+		
+		
 
 	}
 	
