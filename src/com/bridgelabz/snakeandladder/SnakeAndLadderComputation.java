90592fa47e3dd35d69bfdb7d1f52ca3431518abe
rajshetye11 <rajshetye11@gmail.com>
2021-08-09 17:26:42 +0530

    [raj] : Added initial setup for Snake and Ladder

---
+++ src/com/bridgelabz/snakeandladder/SnakeAndLadderComputation.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.snakeandladder;
+
+public class SnakeAndLadderComputation 
+{
+	public static void main(String[] args) {
+
+		System.out.println("Welcome To Snake and Ladder");
+
+	}
+	
+}
